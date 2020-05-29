8c09a0ab09120f8b35a60b0798021d3defb4c391
p-ekul <luke1682@gmail.com>
2020-05-29 09:43:36 +0900

    Create quicks.py

---
+++ quicks/quicks/quicks.py
@@ -0,0 +1,13 @@
+def quick_sort(list):
+	if len(list) <= 1 : return list
+	pivot = list[len(list) // 2]
+	less_arr,equal_arr,big_arr = [],[],[]
+	for i in list:
+		if i < pivot : less_arr.append(i)
+		elif i>pivot : big_arr.append(i)
+		else: equal_arr.append(i)
+	return quick_sort(less_arr) + equal_arr + quick_sort(big_arr)
+	
+list = [11,6,2,8,7,9,5,14,22,34]
+print("quicksorted", quick_sort(list))
+	
